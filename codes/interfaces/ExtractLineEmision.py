961460aaa0337b3d4ebfeac6acf3a562c057d97f
renatosanz <renato.sanchez3103@gmail.com>
2024-07-07 16:27:53 -0600

    add line emision to report

--- codes/interfaces/ExtractLineEmision.py
+++ codes/interfaces/ExtractLineEmision.py
@@ -1,7 +1,7 @@
 import dearpygui.dearpygui as dpg
 import numpy as np
 from scipy.optimize import curve_fit
-import math
+from math import log
 from scipy.integrate import quad
 
 
@@ -25,36 +25,44 @@ class ExtractLineEmision:
             label="Extract Line Emision " + self.datacube.plate_ifu,
             tag=self.datacube.plate_ifu + "LineEmision",
             no_collapse=True,
-            min_size=[600, 800],
+            min_size=[500, 500],
             pos=[0, 0],
             on_close=self.close,
         ):
-            with dpg.plot(width=-1, label="", tag="espectrum_v2", crosshairs=True):
-                dpg.bind_colormap(dpg.last_item(), dpg.mvPlotColormap_Spectral)
-                dpg.add_plot_legend()
-                dpg.add_plot_axis(dpg.mvXAxis, label="Wavelength Å", tag="x_axis_v2")
-                dpg.add_plot_axis(
-                    dpg.mvYAxis,
-                    label=self.datacube.getHeaderInfo("FLUX", "BUNIT"),
-                    tag="y_axis_v2",
-                )
-                dpg.add_heat_series(
-                    x=self.img,
-                    cols=self.datacube.shape[1],
-                    rows=self.datacube.shape[2],
-                    tag="lineEmisionImage",
-                    parent="y_axis_v2",
-                    format="",
-                )
-            dpg.add_button(label="Back", callback=self.close)
-            dpg.bind_item_theme(dpg.last_item(), "exit_btn_theme")
+            with dpg.group(horizontal=True):
+                dpg.add_colormap_scale(tag="colormap_scale",
+                                height=400,
+                                max_scale=max(self.img),
+                                min_scale=min(self.img))
+                dpg.bind_colormap(dpg.last_item(),
+                            dpg.mvPlotColormap_Spectral)
+            
+                with dpg.plot(width=400, label="", tag="espectrum_v2", height=400,crosshairs=True):
+                    dpg.bind_colormap(dpg.last_item(), dpg.mvPlotColormap_Spectral)
+                    dpg.add_plot_legend()
+                    dpg.add_plot_axis(dpg.mvXAxis, label="Wavelength Å", tag="x_axis_v2")
+                    dpg.add_plot_axis(
+                        dpg.mvYAxis,
+                        label=self.datacube.getHeaderInfo("FLUX", "BUNIT"),
+                        tag="y_axis_v2",
+                    )
+                    dpg.add_heat_series(
+                        x=self.img,
+                        cols=self.datacube.shape[1],
+                        rows=self.datacube.shape[2],
+                        tag="lineEmisionImage",
+                        parent="y_axis_v2",
+                        format="",
+                    )
+                dpg.add_button(label="Back", callback=self.close)
+                dpg.bind_item_theme(dpg.last_item(), "exit_btn_theme")
 
-            dpg.configure_item(
-                "lineEmisionImage",
-                x=self.img,
-                scale_max=max(self.img),
-                scale_min=min(self.img),
-            )
+                dpg.configure_item(
+                    "lineEmisionImage",
+                    x=self.img,
+                    scale_max=max(self.img),
+                    scale_min=min(self.img),
+                )
 
     def updateImage(self):
         flux = self.datacube.getFluxData()
@@ -80,18 +88,19 @@ class ExtractLineEmision:
                     max_element_index = np.argmax(espectro)
                     max_val = np.max(espectro)
 
-                    wv_ini = int(wave[max_element_index - 30])
-                    wv_fin = int(wave[max_element_index + 30])
+                    wv_ini = int(wave[max_element_index - 20])
+                    wv_fin = int(wave[max_element_index + 20])
 
-                    continuo_ini = int(wave[max_element_index - 15])
-                    continuo_fin = int(wave[max_element_index + 15])
+                    continuo_ini = int(wave[max_element_index - 10])
+                    continuo_fin = int(wave[max_element_index + 10])
 
                     t = np.where((wave > wv_ini) & (wave < wv_fin))
                     wv = wave[t]
                     sp = espectro[t]
-                    continuo_avr = np.mean(
-                        espectro[wv_ini:continuo_ini] + espectro[continuo_fin:wv_fin]
-                    )
+                    continuo_avr = (
+                        np.mean(espectro[wv_ini:continuo_ini])
+                        + np.mean(espectro[continuo_fin:wv_fin])
+                    ) / 2
 
                     res = ajuste(wv, sp, max_val, wave[max_element_index])
                     if res is not None:
@@ -100,14 +109,19 @@ class ExtractLineEmision:
 
                         # Calcular la integral de la gaussiana ajustada
                         integral, _ = quad(
-                            gaussiana, wv[0], wv[-1], args=(res[0], res[1], res[2])
+                            gaussiana, wv[10], wv[30], args=(res[0], res[1], res[2])
                         )
 
-                    self.img.append(max_val)
-                    print(f"{i,j} : listo - avr : {continuo_avr}")
+                        self.img.append(integral)
+                        print(f"{i,j} : listo - avr : {continuo_avr}")
                 except:
                     self.img.append(0)
                     print(f"{i,j} : listo")
+                self.img=self.applyFunction(self.img)
 
     def close(self):
         dpg.delete_item(self.datacube.plate_ifu + "LineEmision")
+
+    def applyFunction(self, array):
+        l = [log(x, 10) if x > 0 else x for x in array]
+        return l
--- codes/tests/test1.py
+++ codes/tests/test1.py
@@ -11,22 +11,23 @@ def gaussiana(x, a, x0, sigma):
 
 def ajuste(wv, sp, max_val, index_max_val):
     try:
-        popt, pconv = curve_fit(gaussiana, wv, sp, p0=[int(max_val), index_max_val, 10])
+        popt, pconv = curve_fit(gaussiana, wv, sp, p0=[int(max_val), index_max_val, 30])
         return popt
     except RuntimeError as e:
         print(f"Error en el ajuste: {e}")
         return None
 
 
-# Cargar los datos del archivo FITS
-nombre = "../manga-7495-6102-LINCUBE.fits.gz"
+# Cargar los datos del archivo FITS (cubo de datos)
+nombre = "../../../datacubes/manga-7495-6102-LINCUBE.fits.gz"
 hdu = fits.open(nombre)
 flujos = hdu["FLUX"].data
 wave = hdu["WAVE"].data
-espectro = flujos[:, 27, 27]
+espectro = flujos[:, 20, 20]
 espectro = np.nan_to_num(espectro)
 
-hdul_catalog = fits.open("../redshift_catalog/dapall-v3_1_1-3.1.0.fits")
+# Cargar los datos del archivo FITS (catalogo de redshift)
+hdul_catalog = fits.open("../../../datacubes/redshift_catalog/dapall-v3_1_1-3.1.0.fits")
 data_catalog = hdul_catalog[1].data
 ix = np.where(data_catalog["PLATEIFU"] == hdu["FLUX"].header["PLATEIFU"])
 redshift = data_catalog["nsa_z"][ix][0]
@@ -36,18 +37,23 @@ wave = wave / (1 + redshift)
 max_element_index = np.argmax(espectro)
 max_val = np.max(espectro)
 
-wv_ini = int(wave[max_element_index - 30])
-wv_fin = int(wave[max_element_index + 30])
+margin = 20
+continuo_margin = int(margin * 0.5)
 
-continuo_ini = int(wave[max_element_index - 15])
-continuo_fin = int(wave[max_element_index + 15])
+wv_ini = int(wave[max_element_index - margin])
+wv_fin = int(wave[max_element_index + margin])
+
+continuo_ini = int(wave[max_element_index - continuo_margin])
+continuo_fin = int(wave[max_element_index + continuo_margin])
 
 print(f"{wv_ini}---{continuo_ini}--A--{continuo_fin}---{wv_fin}")
 
 t = np.where((wave > wv_ini) & (wave < wv_fin))
 wv = wave[t]
 sp = espectro[t]
-continuo_avr = np.mean(espectro[wv_ini:continuo_ini] + espectro[continuo_fin:wv_fin])
+continuo_avr = (
+    np.mean(espectro[wv_ini:continuo_ini]) + np.mean(espectro[continuo_fin:wv_fin])
+) / 2
 
 res = ajuste(wv, sp, max_val, wave[max_element_index])
 if res is not None:
@@ -57,13 +63,28 @@ if res is not None:
     print(f"gaussiana = {gaus}")
 
     # Calcular la integral de la gaussiana ajustada
-    integral, _ = quad(gaussiana, wv[0], wv[-1], args=(res[0], res[1], res[2]))
+    integral, _ = quad(gaussiana, wv[10], wv[30], args=(res[0], res[1], res[2]))
 
     flg = plt.figure()
     ax = flg.add_subplot(111)
-    ax.plot(wv, sp)
-    ax.plot(wv, gaus + continuo_avr)
-    ax.set_title(f"integral: {integral}")
+    ax.set_xlabel("Longitud de onda ")
+    ax.set_ylabel(hdu["FLUX"].header["BUNIT"])
+    ax.plot(wv, sp, color="#26854c",label = "Espectro")
+    ax.plot(wv, gaus + continuo_avr, color="#ec273f",label = "Campana Gauss")
+    ax.set_title(f"integral: {integral} : (20,20)")
+    ax.vlines(
+        wave[max_element_index],
+        ymin=0,
+        colors="#de5d3a",
+        linestyles="dashdot",
+        ymax=max_val,
+        label="l",
+    )
+    ax.vlines(continuo_ini, ymin=0, colors="#26854c", ymax=max_val, label="l-p")
+    ax.vlines(continuo_fin, ymin=0, colors="#26854c", ymax=max_val, label="l+p")
+    plt.fill_between(wv, gaus + continuo_avr, color="#26854c83")
+    plt.grid()
+    plt.legend()
     plt.show()
 else:
     print("No se pudo ajustar la curva.")

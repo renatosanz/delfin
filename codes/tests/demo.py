75f3f6baf95e813ea2a7818a1f0899e6362b1e86
renatosanz <renato.sanchez3103@gmail.com>
2024-07-11 17:52:47 -0600

    lineas de emision corregidos

--- codes/tests/demo.py
+++ codes/tests/demo.py
@@ -5,6 +5,8 @@ from scipy.optimize import curve_fit
 from scipy.integrate import quad
 import math
 
+# null value = None
+
 
 def gaussiana(x, a, x0, sigma):
     return a * np.exp(-((x - x0) ** 2) / (2 * sigma**2))
@@ -14,12 +16,13 @@ def ajuste(wv, sp, max_val, index_max_val, sigma):
     popt, pconv = curve_fit(gaussiana, wv, sp, p0=[max_val, index_max_val, sigma])
     return popt
 
+
 def integralGauss(x):
     total = 0
     for n in x:
-        total += math.pow(math.e,math.pow(-n,2))
+        total += math.pow(math.e, math.pow(-n, 2))
     return total
-    
+
 
 # Cargar los datos del archivo FITS
 nombre = "../../../datacubes/manga-7495-6102-LINCUBE.fits.gz"
@@ -38,6 +41,10 @@ wave = wave / (1 + redshift)
 
 lineas = [
     {"nombre": "[OIII]", "x": 5007},
+]
+
+lineas1 = [
+    {"nombre": "[OIII]", "x": 5007},
     {"nombre": "Ha", "x": 6564},
     {"nombre": "[NII]", "x": 6584},
     {"nombre": "[NII]", "x": 6548},
@@ -48,7 +55,7 @@ lineas = [
     {"nombre": "[SII]", "x": 6731},
 ]
 
-for i in lineas:
+for i in lineas1:
     img = np.zeros((img_h, img_w))
     longOnda = i["x"]
     nom = i["nombre"]
@@ -59,18 +66,18 @@ for i in lineas:
             # espectro = np.nan_to_num(espectro)
             # Verificar si el espectro está lleno de ceros
             if np.all(espectro == 0) or np.isinf(espectro).any():
-                img[i, j] = 0
+                img[i, j] = None
                 continue
 
             try:
                 max_element_index = int(longOnda - wave[0])
                 max_val = espectro[max_element_index]
 
-                wv_ini = int(wave[max_element_index - 30])
-                wv_fin = int(wave[max_element_index + 30])
+                wv_ini = int(wave[max_element_index - 150])
+                wv_fin = int(wave[max_element_index + 150])
 
-                continuo_ini = int(wave[max_element_index - 15])
-                continuo_fin = int(wave[max_element_index + 15])
+                continuo_ini = int(wave[max_element_index - 25])
+                continuo_fin = int(wave[max_element_index + 25])
 
                 t = np.where((wave > wv_ini) & (wave < wv_fin))
                 wv = wave[t]
@@ -79,43 +86,32 @@ for i in lineas:
                     np.mean(espectro[wv_ini:continuo_ini])
                     + np.mean(espectro[continuo_fin:wv_fin])
                 ) / 2
+                
+                m = 0.5
                 media = (max_val - continuo_avr) / 2
-                ds = wv[np.where((sp > media - 1) & (sp < media + 1))]
-                
-                print(media, ds)
-                
-                FWAHM = ds[-1] - ds[0]
+                ds = [x if x>media-m or x<media+m else 0 for x in sp]
+                LeftPoint = ds[0]
+                RightPoint = ds[-1]
 
-                res = ajuste(wv, sp, max_val, wave[max_element_index], FWAHM)
+                fwahm = abs(RightPoint - LeftPoint)
+
+                res = ajuste(wv, sp, max_val, wave[max_element_index], fwahm)
                 if res is not None:
-                    gaus = gaussiana(wv, res[0], res[1], res[2])
+                    # gaus = gaussiana(wv, res[0], res[1], res[2])
                     # Calcular la integral de la gaussiana ajustada
                     integral, _ = quad(
                         gaussiana, wv[0], wv[-1], args=(res[0], res[1], res[2])
                     )
 
                 img[i, j] = integral - continuo_avr * (wv[-1] - wv[0])
-                print(f"{i,j} : {img[i,j]} - listo - avr : {continuo_avr}")
+                print(f"{i,j} : {img[i,j]} - listo - fwahm : {fwahm}")
             except:
-                img[i, j] = 0
-                print(f"{i,j} : {img[i,j]} - listo")
-    
-    print(longOnda)
-    # Mostrar un gráfico tipo mapa de calor de los datos generados
-    fig = plt.figure()
-    bx = fig.add_subplot(111)
-    cx = bx.imshow(np.log10(img))
-    plt.colorbar(cx, label="Integral de la Gaussiana")
-    plt.title("Mapa log10 "+str(longOnda)+str(nom))
-    plt.xlabel("X")
-    plt.ylabel("Y")
-    plt.savefig("./imgs/img"+str(longOnda)+str(nom)+"log10.png")
-    
+                img[i, j] = None
     fig = plt.figure()
     bx = fig.add_subplot(111)
     cx = bx.imshow(img)
     plt.colorbar(cx, label="Integral de la Gaussiana")
-    plt.title("Mapa "+str(longOnda)+str(nom))
+    plt.title("Mapa " + str(longOnda) + str(nom))
     plt.xlabel("X")
     plt.ylabel("Y")
-    plt.savefig("./imgs/img"+str(longOnda)+str(nom)+".png")
+    plt.savefig("./imgs/img" + str(longOnda) + str(nom) + ".png")
